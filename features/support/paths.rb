c2d37652f0b03ccc4e2b67adf3fe6abeffec370e
Mike Skalnik <mike.skalnik@gmail.com>
2010-11-17 16:49:40 -0500

    Add mapping for post index path

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -11,6 +11,9 @@ module NavigationHelpers
     when /the home\s?page/
       '/'
 
+    when /the list of posts/
+      posts_path
+
     # Add more mappings here.
     # Here is an example that pulls values out of the Regexp:
     #
