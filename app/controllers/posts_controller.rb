98ab224154ec75c895c06939db2bb294b76cd939
Mike Skalnik <mike.skalnik@gmail.com>
2010-11-17 23:52:47 -0500

    Added Post create action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,10 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    post = Post.new(params[:post])
+    post.save
+    redirect_to :action => 'index'
+  end
 end
