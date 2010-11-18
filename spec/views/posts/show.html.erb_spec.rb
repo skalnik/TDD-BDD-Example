4916278c82d116045fda30d7931807873dd94dbe
Mike Skalnik <mike.skalnik@gmail.com>
2010-11-18 01:09:04 -0500

    Flesh out rest of post/show view spec

--- spec/views/posts/show.html.erb_spec.rb
+++ spec/views/posts/show.html.erb_spec.rb
@@ -1,14 +1,30 @@
 require 'spec_helper'
 
 describe 'posts/show.html.erb' do
+  let(:mock_post) { mock_model(Post, :title => 'Woo tests', :body => 'Test...test...test...') }
+
+  before(:each) do
+    assign :post, mock_post
+  end
+
   it 'displays the post title and body' do
-    assign :post, mock_model(Post, :title => 'Woo tests', :body => 'Test...test...test...')
     render
     rendered.should contain('Woo tests')
     rendered.should contain('Test...test...test...')
   end
 
-  it 'has an edit link'
-  it 'has a link to the list of posts'
-  it 'has a destroy link'
+  it 'has an edit link' do
+    render
+    rendered.should have_selector('a', :content => 'Edit', :href => edit_post_path(mock_post))
+  end
+
+  it 'has a link to the list of posts' do
+    render
+    rendered.should have_selector('a', :content => 'List of Posts', :href => posts_path)
+  end
+
+  it 'has a destroy link' do
+    render
+    rendered.should have_selector('a', :content => 'Destroy')
+  end
 end
