ea8d1eae61444ccc4cc31ddca7f6789beb959517
Mike Skalnik <mike.skalnik@gmail.com>
2010-11-17 22:48:21 -0500

    Added test to ensure that textarea is generated for a new post body

--- spec/views/posts/new.html.erb_spec.rb
+++ spec/views/posts/new.html.erb_spec.rb
@@ -27,4 +27,14 @@ describe 'posts/new.html.erb' do
         :value => 'Awesome title')
     end
   end
+
+  it 'renders a text area for the post body' do
+    post.body = 'Testing ftw'
+    render
+    rendered.should have_selector('form') do |form|
+      form.should have_selector('textarea',
+        :name => 'post[body]',
+        :content => 'Testing ftw')
+    end
+  end
 end
