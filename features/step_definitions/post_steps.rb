3d04520cef49791c451ab6c9ef8a03bd59f0abb3
Mike Skalnik <mike.skalnik@gmail.com>
2010-11-17 20:39:39 -0500

    Step definition for simulated browser

--- features/step_definitions/post_steps.rb
+++ features/step_definitions/post_steps.rb
@@ -4,3 +4,9 @@ Given /^posts titled (.+)$/ do |titles|
   end
 end
 
+When /^I create a new post titled "([^"]*)" with body "([^"]*)"$/ do |title, body|
+  visit new_post_path
+  fill_in 'Title', :with => title
+  fill_in 'Body', :with => body
+  click_button 'Save'
+end
