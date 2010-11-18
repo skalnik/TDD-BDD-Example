237e7d2c17f38039f8518c1cc65108c8b8bc392a
Mike Skalnik <mike.skalnik@gmail.com>
2010-11-17 23:10:43 -0500

    Started on controller spec for new post creation

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -12,4 +12,22 @@ describe PostsController do
       get 'index'
     end
   end
+
+  describe 'GET new' do
+    it 'should be successful' do
+      get 'new'
+      response.should be_success
+    end
+
+    it 'should create a new post object' do
+      Post.should_receive(:new)
+      get 'new'
+    end
+  end
+
+  describe 'POST create' do
+    it 'should create a new message'
+    it 'should save the message'
+    it 'should redirect to the posts index'
+  end
 end
