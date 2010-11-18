0eef8b9369781ccda5feb215c12d66c83c0fd5e6
Mike Skalnik <mike.skalnik@gmail.com>
2010-11-17 22:32:18 -0500

    Added test to ensure a text field is created for the post title

--- spec/views/posts/new.html.erb_spec.rb
+++ spec/views/posts/new.html.erb_spec.rb
@@ -1,6 +1,12 @@
 require 'spec_helper'
 
 describe 'posts/new.html.erb' do
+  let(:post) { stub_model(Post).as_new_record }
+
+  before(:each) do
+    assign(:post, post)
+  end
+
   it 'renders a form to create a new post' do
     render
     rendered.should have_selector('form',
@@ -8,6 +14,17 @@ describe 'posts/new.html.erb' do
       :action => posts_path
     ) do |form|
       form.should have_selector('input', :type => 'submit')
+    end
+  end
+
+  it 'renders a text field for the post title' do
+    post.title = 'Awesome title'
+    render
+    rendered.should have_selector('form') do |form|
+      form.should have_selector('input',
+        :type => 'text',
+        :name => 'post[title]',
+        :value => 'Awesome title')
     end
   end
 end
